c3cee97e4de6bc0d32fd8a8a533cbcd0631cfa08
root <root@DESKTOP-6P7DQHA.localdomain>
2021-07-05 23:00:16 +0530

    added test 04

---
+++ auto/test04/test04.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main () {
+
+std::vector <int> first_vector = {1,2,3,4,34,34,43};
+
+std::cout << "This is the First vector :" << first_vector.front() << "\n this is the last one of the vector :" << first_vector.back();
+
+
+
+}
