b094619cc6756c8cfc8d222e5c3f27b1fdc07e58
root <root@DESKTOP-6P7DQHA.localdomain>
2021-07-06 18:07:29 +0530

    Updated

--- auto/test04/test04.cpp
+++ auto/test04/test04.cpp
@@ -6,7 +6,6 @@ int main () {
 std::vector <int> first_vector = {1,2,3,4,34,34,43};
 
 std::cout << "This is the First vector :" << first_vector.front() << "\n this is the last one of the vector :" << first_vector.back();
-
-
+first_vector.push_back(23);
 
 }
---
+++ auto/test05/test05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main (){
+
+ int number = 2 ; 
+ int& number_ref = number ; 
+ number = 56 ; 
+ std::cout << "new ref :" << number << std::endl ;  
+
+
+
+
+
+
+}
